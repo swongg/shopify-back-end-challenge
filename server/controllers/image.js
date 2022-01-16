f23d4427fd087ff085c3e651df8e7c378cd5a4d9
steven wong <wong.steven604@gmail.com>
2022-01-15 19:11:47 -0800

    create upload image endpoint and sync with s3 bucket

---
+++ server/controllers/image.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1",
+});
+
+const upload = multer({
+  storage: multerS3({
+    acl: 'public-read',
+    s3: s3,
+    bucket: "shopify-2022-inventory-images",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+export { upload };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 import item from "./item";
+import image from "./image";
 import express from "express";
 
 const router = express.Router();
 router.use("/item", item);
+router.use("/image", image);
 
 export default router;
